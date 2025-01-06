d24b7cd0490c24093b23e34f640f26108ac06f74
khcjump <khcjump@gmail.com>
2025-01-07 00:12:44 +0800

    ok

---
+++ demo/src/main/java/com/example/demo/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+  @RequestMapping("/check")
+  public String check() {
+    return "OK";
+  }
+}
